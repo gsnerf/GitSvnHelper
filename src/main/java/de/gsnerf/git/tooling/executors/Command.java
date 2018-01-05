7b596b2ce0e23ae968f71f8daef7a47e97ec662f
Michalis Adamidis <gsnerf@gsnerf.de>
2018-01-05 21:34:54 +0100

    Extended command to be able to specify where commands are executed.

--- src/main/java/de/gsnerf/git/tooling/executors/Command.java
+++ src/main/java/de/gsnerf/git/tooling/executors/Command.java
@@ -1,5 +1,6 @@
 package de.gsnerf.git.tooling.executors;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
@@ -15,12 +16,25 @@ public class Command {
     }
     
     /**
+     * Execution happens in the current directory.
      * @param command {@link List} representing the command to execute. Use a new entry for each parameter and the command itself.
      * @return A {@link List} of Strings containing all unprocessed lines created as output by the command.
      * @throws IOException when some problem occured while reading the output
      */
     public static CommandResult execute( List<String> command ) throws IOException {
-        Process process = new ProcessBuilder( command ).start();
+        return execute(new File("."), command);
+    }
+    
+    /**
+     * @param workingDirectory the {@link File} pointing to the directory the command should be executed in
+     * @param command {@link List} representing the command to execute. Use a new entry for each parameter and the command itself.
+     * @return A {@link List} of Strings containing all unprocessed lines created as output by the command.
+     * @throws IOException when some problem occured while reading the output
+     */
+    public static CommandResult execute( File workingDirectory, List<String> command ) throws IOException {
+        ProcessBuilder builder = new ProcessBuilder( command );
+        builder.directory( workingDirectory );
+        Process process = builder.start();
         return new CommandResult(process);
     }
 
